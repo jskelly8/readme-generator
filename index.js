61cd10284b0c1310edf03cbbb108824abbf0a36b
Jess Skelton <jessskelton888@gmail.com>
2023-12-16 15:29:42 -0600

    edited fields in package.json, added a line to gitignore file, & began variables and functions on index.js

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer'); // Version 8.2.4, shown on line 12 of package.json
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+// const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: '',
+        name: '',
+        message: ''
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// Function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName, data));
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+// Function to initialize app
+// function init() {
+//     inquirer.prompt(questions).then((responses) -> {
+//         console.log('Creating a professional README.md file...');
+//         writeToFile('./README.md', generateMarkdown({...responses});)
+//     });
+// };
 
 // Function call to initialize app
 init();
