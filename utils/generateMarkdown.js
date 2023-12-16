9481dddd2509d13699d18c8cdb52e0ecb1fe1969
Jess Skelton <jessskelton888@gmail.com>
2023-12-16 16:12:18 -0600

    Added README md content to generateMarkdown function on generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+  ${renderLicenseBadge(data.license)}
+  // other badge render function???
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Badges](#badges)
+  - [Features](#features)
+  - [How to Contribute](#how to Contribute)
+  - [Tests](#tests)
+  - [Questions?](#questions?)
+
+  ## Installation
+  ${data.installationInstruction}
+
+  ## Usage
+  ${data.usage}
+
+  ## Credits
+  ${data.credits}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Badges
+  ${data.badges}
+
+  ## Features
+  ${data.features}
+
+  ## How to Contribute
+  ${data.contribute}
+
+  ## Tests
+  ${data.tests}
+
+  ## Questions?
+  If you have any questions, feel free to reach out to me via [GitHub](https://github.com/${data.gitHubUser}/) or email me at ${data.email}.
 
 `;
 }
